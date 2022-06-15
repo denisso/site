d1e7cecd91844e3df4e4ec649a467bfb7ae6455e
Denis <like-nix@mail.ru>
2022-06-16 02:08:04 +0300

    page about

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -1,36 +1,38 @@
 import React from "react";
 import { scrollTo, AnimateItem } from "components/Tools";
 import styled from "styled-components";
-import {
-    faHand
-} from "@fortawesome/free-regular-svg-icons";
+import { faHand } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ImageLazy } from "components/Elements/ImageLazy";
+
 const Content = styled.div`
-    .Icon {
-        margin-right: 0.5rem;
-    }
     h1 {
         text-align: center;
         margin-bottom: 1rem;
+        .Icon {
+            margin-right: 0.5rem;
+        }
     }
 `;
 
 export const AboutMe = () => {
+
     React.useEffect(() => {
         document.title = "About me";
         scrollTo(0);
     }, []);
+
     return (
         <AnimateItem>
             <Content>
-                <h1><FontAwesomeIcon icon={faHand} className="Icon"/>About Me</h1>
-                <span>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Veritatis laudantium rerum ab facilis dicta fugit, cum
-                    repellendus. Eius tempore laudantium, explicabo enim
-                    necessitatibus, consequatur quasi, doloribus rerum cum
-                    similique veritatis.
-                </span>
+                <h1>
+                    <FontAwesomeIcon icon={faHand} className="Icon" />
+                    About Me
+                </h1>
+                <div className="HeroBlock">
+
+                    <ImageLazy src="/asset/Face.jpg"  width="1080" height="1080" alt="hero of this site"/>
+                </div>
             </Content>
         </AnimateItem>
     );
