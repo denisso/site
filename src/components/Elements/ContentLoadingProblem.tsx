8c1c503a59d24cafdf73bcd80e444b3aec0d9077
Denis <like-nix@mail.ru>
2022-07-03 10:03:06 +0300

    Placeholder for data empty or error block

---
+++ src/components/Elements/ContentLoadingProblem.tsx
@@ -0,0 +1,46 @@
+/**
+ * @description Placeholder for data empty or error block
+ * @author Denis Kurochkin (mr_dramm) <blackbrain2009@gmail.com>
+ * @copyright Denis Kurochkin 2022
+ */
+
+import { faFaceSadCry } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 3rem 0;
+    color: ${({ theme }) => theme.colors.firstLight};
+    .Icon {
+        width: 3rem;
+        height: auto;
+    }
+    .Message {
+        font-size: 1.4rem;
+        font-weight: bold;
+        margin: 2rem 0;
+        text-align: center;
+    }
+`;
+
+export const ContentLoadingProblem = ({ message }: { message: string }) => {
+    return (
+        <Container>
+            <FontAwesomeIcon className="Icon" icon={faFaceSadCry} />
+            <div className="Message">{message}</div>
+        </Container>
+    );
+};
+
+export const ContentLoadingProblemNotFound = () => {
+    return (
+        <ContentLoadingProblem message="Could not find data for such a query..." />
+    );
+};
+
+export const ContentLoadingProblemError = () => {
+    return <ContentLoadingProblem message="An error occurred during loading..." />;
+};
